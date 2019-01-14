408c2636bb9da0446ce67d3407e190e3bf1d4e50
klkirstyleeskl <k.lees@btinternet.com>
2019-01-14 17:52:34 +0000

    sets up form_view file

--- hw_pubsub_prime_checker_start/src/app.js
+++ hw_pubsub_prime_checker_start/src/app.js
@@ -1,3 +1,6 @@
+const FormView = require('./views/form_view.js');
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript Loaded');
+  const formView = new FormView();
+  formView.bindEvents();
 });
